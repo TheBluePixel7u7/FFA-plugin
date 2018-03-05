3447458b207e48594de2f1e1d2b30e8670fb98a3
TheBluePixel <thebluepixelmc@gmail.com>
2018-03-04 21:35:18 -0600

    Update Loader.php

--- src/FFA/Loader.php
+++ src/FFA/Loader.php
@@ -1,1 +1,22 @@
+<?php
+namespace FFA;
 
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerItemHeldEvent;
+// commands
+use FFA\Commands\SelectMapCommand;
+
+class Loader extends PluginBase implements Listener{
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+		$this->getLogger()->info("Enabled.");
+		$this->getServer()->getCommandMap()->register("select", new SelectMapCommand($this));
+	}
+  public function registerCommands(){
+	    $this->getCommand("select")->setExecutor(new SelectMapCommand($this));
+	}
+}
