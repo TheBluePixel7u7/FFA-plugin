88ad35ee7dd1d131e9c8a34eda45d67f7a36e30c
TheBluePixel <thebluepixelmc@gmail.com>
2018-03-13 17:55:59 -0600

    Update SelectMapCommand.php

--- src/FFA/Commands/SelectMapCommand.php
+++ src/FFA/Commands/SelectMapCommand.php
@@ -13,35 +13,49 @@ use pocketmine\level\sound\EndermanTeleportSound;
 use pocketmine\utils\TextFormat;
 // Centro xd
 use FFA\Loader;
+use FFA\SlidePlayer;
+use FFA\Tasks\Task;
 
 class SelectMapCommand extends PluginCommand{
 	
 	public function __construct(Loader $plugin){
 	parent::__construct("select", $plugin);
 	$this->setDescription("Select ffa arena Command");
-	$this->setAliases(["selectmap", "smap"]);
+	$this->setAliases(["ffa", "smap"]);
 	$this->base = $plugin;
 }
+       // based in SoupFFA by McpeBooster
     public function execute(CommandSender $sender, $label, array $args){
 		if(empty($args[0])){
-		$sender->sendMessage("§bFFA§6Core§7> §9Use /select <map> \n §a
-}Avaliable maps: §f[1, 2, 2]");
+		$sender->sendMessage("§bFFA§6Core§7> §9Use /select <1> \n §a
+}Avaliable maps: §f[1]");
 	return false;
 	}
-	if($args[0] == "1"){
-
-$name = $this->base->getConfig()->get("ffacore.world1.name");//conseguir el nombre del mundo
-
-if(!$this->base->getServer()->isLevelLoaded($name)){//verificar que el mundo no este cargado
-$this->base->getServer()->loadLevel($name);//cargar si no lo esta
-}
-$world = $this->base->getServer()->getLevelByName($name);
-//var_dump($world);
-if($world instanceof Level){//verificar si el mundo es válido
-$sender->teleport($world->getSafeSpawn());
-}else{
-$sender->sendMessage("Error");//error en caso que no lo sea
-}
+    if($args[0] == "1"){
+		 
+		 $player = $sender;
+         $world = $sender->getLevel()->getFolderName();
+         $arena = $this->base->getConfig()->get("ffacore.player.join");
+         if ($arena == $world) {
+             $player->sendMessage($this->base->getConfig()->get("ffacore.player.join"));
+             return false;
+                    }
+                    if ($arena == $world){
+                        $player->sendMessage($this->base->getConfig()->get("ffacore.arena.player.isalready"));
+                        return false;
+                    } else {
+                        $player->PlayerJoin();
+						$player->Task();
+                        return true;
+                    }
+	        }elseif ($args[0] == "leave" or $args[0] == "quit"){
+					$player = $sender;
+                    $world = $player->getLevel()->getFolderName(); // que sea un mundo xd
+                    $arena = $this->base->getConfig()->get("ffacore.player.leave");
+                    if ($arena == $world) {
+                        $player->PlayerLeave();
+                        return true;
+                    }
 	}
 }
 }
